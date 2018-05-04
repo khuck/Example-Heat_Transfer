15bb12594d36d615eebf0b3688d254f422619582
Kevin Huck <khuck@cs.uoregon.edu>
2018-05-04 08:03:57 -0700

    Adding Titan readme

---
+++ test_sos/README-Titan.sh
@@ -0,0 +1,54 @@
+# How to run this example on titan.ccs.ornl.gov:
+
+## Environmenet setup
+
+Source the CODAR build environment shell script:
+
+```bash
+source /lustre/atlas/proj-shared/csc249/CSC249ADCD01/software2/sourceme-titan.sh
+```
+
+## Build the software:
+
+Build the heat_transfer_adios2 and stage_write examples:
+
+```
+cd Example-Heat_Transfer
+make TAU=1
+cd stage_write
+make TAU=1
+cd ..
+```
+
+## Set up and run the example:
+
+Make sure that both heat_transfer.xml and run_cori.slurm specify the same
+ADIOS transfer method (either DATASPACES or FLEXPATH).
+
+```
+cd test_sos
+cp ../heat_transfer.xml .
+cp ../dataspaces.conf .
+sbatch run_cori.slurm
+```
+
+## To add ADIOS profile/trace extraction:
+
+In the test_sos directory, clone the SOS scripts repo:
+
+```
+cd test_sos
+git submodule update --recursive --remote
+git submodule update --init sos_flow_experiments
+git submodule foreach git pull origin master
+```
+
+Then run the example set up for profile extraction or trace extraction (making
+sure that the same ADIOS transfer method is set, similar to previous example):
+
+```bash
+# For profile collection:
+sbatch run_titan_profile.slurm
+# For trace collection:
+sbatch run_titan_trace.slurm
+```
